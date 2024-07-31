4a980f11128debc5cea363eec993793082590d3b
Palmer <reyflorian3@gmail.com>
2024-07-31 20:12:10 +0200

    fix: tab-rounded + tab-types

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,10 +1,9 @@
-import { router, Tabs } from "expo-router";
-import React, { useContext } from "react";
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import { Colors } from "@/constants/Colors";
-import useColorScheme, { ThemeContext } from "@/hooks/useColorScheme";
-import { useAppSelector } from "@/hooks/store";
-import { type UserState } from "@/types";
+import {router, Tabs} from "expo-router";
+import React, {useContext} from "react";
+import {TabBarIcon} from "@/components/navigation/TabBarIcon";
+import useColorScheme, {ThemeContext} from "@/hooks/useColorScheme";
+import {useAppSelector} from "@/hooks/store";
+import {type UserState} from "@/types";
 // Import light school logo
 import Webtech_light from "@/assets/images/school/light/Webtech_logo-min_light.png";
 import Escen_light from "@/assets/images/school/light/Escen_logo-min_light.png";
@@ -18,230 +17,211 @@ import Magnum_dark from "@/assets/images/school/dark/Magnum_logo-min_dark.png";
 import Bachelor_dark from "@/assets/images/school/dark/Bachelor_logo-min_dark.png";
 import Atlas_dark from "@/assets/images/school/dark/Atlas_logo-min_dark.png";
 // TODO - remove TMP
-import { Image, TouchableOpacity } from "react-native";
-import {
-  AwardIcon,
-  BellIcon,
-  BookIcon,
-  BurgerMenuIcon,
-} from "@/components/IconComponent";
-import { colors } from "@/config/styles/01-settings/_colors";
+import {Image, TouchableOpacity} from "react-native";
+import {AwardIcon, BellIcon, BookIcon, BurgerMenuIcon,} from "@/components/IconComponent";
+import {colors} from "@/config/styles/01-settings/_colors";
 import styles from "@/styles/Navigation";
 import TabBar from "@/components/TabBar/TabBar";
 
 export default function TabLayout() {
-  const themeContext = useContext(ThemeContext);
-  const colorScheme = useColorScheme();
-  const isDark = colorScheme === "dark";
-  const userInfos: UserState = useAppSelector((state) => state.userInfos);
-  const logoToDisplay = {
-    "WebTech Institute": isDark ? Webtech_dark : Webtech_light,
-    "ATLAS Institute": isDark ? Atlas_dark : Atlas_light,
-    "Magnum Institute": isDark ? Magnum_dark : Magnum_light,
-    "Bachelor Institute": isDark ? Bachelor_dark : Bachelor_light,
-    "ESCEN": isDark ? Escen_dark : Escen_light,
-    "NEXT-U": isDark ? Escen_dark : Escen_light,
-  };
+    const themeContext = useContext(ThemeContext);
+    const colorScheme = useColorScheme();
+    const isDark = colorScheme === "dark";
+    const userInfos: UserState = useAppSelector((state) => state.userInfos);
+    const logoToDisplay = {
+        "WebTech Institute": isDark ? Webtech_dark : Webtech_light,
+        "ATLAS Institute": isDark ? Atlas_dark : Atlas_light,
+        "Magnum Institute": isDark ? Magnum_dark : Magnum_light,
+        "Bachelor Institute": isDark ? Bachelor_dark : Bachelor_light,
+        "ESCEN": isDark ? Escen_dark : Escen_light,
+        "NEXT-U": isDark ? Escen_dark : Escen_light,
+    };
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint, // Sert à rien
-        tabBarActiveBackgroundColor: "red", // Marche pas
-        tabBarInactiveTintColor: "red", // Marche pas
-        tabBarInactiveBackgroundColor: "red", // Marche pas
-        headerStyle: {
-          // Marche pas
-          backgroundColor: "blue",
-        },
-      }}
-      sceneContainerStyle={{
-        // Marche pas
-        backgroundColor: "green", // Marche pas
-        borderWidth: 4, // Debug
-      }}
-      tabBar={TabBar}
-    >
-      <Tabs.Screen
-        name="home"
-        options={{
-          headerTitleAlign: "center",
-          headerShown: true,
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "home" : "home-outline"}
-              color={color}
-              focused={focused}
+    return (
+        <Tabs
+            screenOptions={{}}
+            tabBar={TabBar}
+        >
+            <Tabs.Screen
+                name="home"
+                options={{
+                    headerTitleAlign: "center",
+                    headerShown: true,
+                    tabBarIcon: ({color, focused}) => (
+                        <TabBarIcon
+                            name={focused ? "home" : "home-outline"}
+                            color={color}
+                            focused={focused}
+                        />
+                    ),
+                    headerTitle: () => (
+                        <Image
+                            source={logoToDisplay[userInfos?.school?.name ?? "ESCEN"]} // School Icon from the user
+                            style={styles.schoolLogo}
+                        />
+                    ),
+                    headerLeft: () => (
+                        <TouchableOpacity
+                            style={[styles.headerBtnContainer]}
+                            onPress={() => router.navigate("/(app)/(screens)/leaderboard")}
+                            hitSlop={{top: 25, bottom: 25, left: 25, right: 25}}
+                        >
+                            <AwardIcon
+                                size={20}
+                                color={themeContext?.isDark ?? false ? "white" : "black"}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerRight: () => (
+                        <TouchableOpacity
+                            style={[styles.headerBtnContainer]}
+                            onPress={() => router.navigate("/(app)/(screens)/notification")}
+                            hitSlop={{top: 25, bottom: 25, left: 25, right: 25}}
+                        >
+                            <BellIcon
+                                size={20}
+                                color={themeContext?.isDark ?? false ? "white" : "black"}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerStyle: {
+                        backgroundColor:
+                            (themeContext?.isDark
+                                ? themeContext?.colors.primarySemiDark
+                                : themeContext?.colors.primarySemiLight) ?? "red",
+                    },
+                }}
             />
-          ),
-          headerTitle: () => (
-            <Image
-              source={logoToDisplay[userInfos?.school?.name ?? "ESCEN"]} // School Icon from the user
-              style={styles.schoolLogo}
+            <Tabs.Screen
+                name="list-form"
+                options={{
+                    title: "Formulaire",
+                    headerTitleAlign: "center",
+                    headerShown: true,
+                    tabBarIcon: ({color, focused}) => (
+                        <TabBarIcon
+                            name={focused ? "clipboard" : "clipboard-outline"}
+                            color={color}
+                            focused={focused}
+                        />
+                    ),
+                    headerRight: () => {
+                        return (
+                            <TouchableOpacity
+                                style={[styles.headerBtnContainer]}
+                                onPress={() => router.navigate("/(app)/forms/info-school")}
+                                hitSlop={{top: 25, bottom: 25, left: 25, right: 25}}
+                            >
+                                <BookIcon
+                                    size={20}
+                                    color={themeContext?.isDark ?? false ? "white" : "black"}
+                                />
+                            </TouchableOpacity>
+                        );
+                    },
+                    headerStyle: {
+                        backgroundColor:
+                            (themeContext?.isDark
+                                ? themeContext?.colors.primarySemiDark
+                                : themeContext?.colors.primarySemiLight) ?? "red",
+                    },
+                    headerTitleStyle: {
+                        fontSize: 17,
+                        fontFamily: "Raleway-Bold",
+                        color: themeContext?.isDark ? colors.textDark : colors.textLight,
+                    },
+                }}
             />
-          ),
-          headerLeft: () => (
-            <TouchableOpacity
-              style={[styles.headerBtnContainer]}
-              onPress={() => router.navigate("/(app)/(screens)/leaderboard")}
-              hitSlop={{ top: 25, bottom: 25, left: 25, right: 25 }}
-            >
-              <AwardIcon
-                size={20}
-                color={themeContext?.isDark ?? false ? "white" : "black"}
-              />
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <TouchableOpacity
-              style={[styles.headerBtnContainer]}
-              onPress={() => router.navigate("/(app)/(screens)/notification")}
-              hitSlop={{ top: 25, bottom: 25, left: 25, right: 25 }}
-            >
-              <BellIcon
-                size={20}
-                color={themeContext?.isDark ?? false ? "white" : "black"}
-              />
-            </TouchableOpacity>
-          ),
-          headerStyle: {
-            backgroundColor:
-              (themeContext?.isDark
-                ? themeContext?.colors.primarySemiDark
-                : themeContext?.colors.primarySemiLight) ?? "red",
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="list-form"
-        options={{
-          title: "Formulaire",
-          headerTitleAlign: "center",
-          headerShown: true,
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "clipboard" : "clipboard-outline"}
-              color={color}
-              focused={focused}
+            <Tabs.Screen
+                name="archive"
+                options={{
+                    title: "Archive",
+                    headerTitleAlign: "center",
+                    tabBarIcon: ({color, focused}) => (
+                        <TabBarIcon
+                            name={focused ? "archive" : "archive-outline"}
+                            color={color}
+                            focused={focused}
+                        />
+                    ),
+                    headerStyle: {
+                        backgroundColor:
+                            (themeContext?.isDark
+                                ? themeContext?.colors.primarySemiDark
+                                : themeContext?.colors.primarySemiLight) ?? "red",
+                    },
+                    headerTitleStyle: {
+                        fontSize: 17,
+                        fontFamily: "Raleway-Bold",
+                        color: themeContext?.isDark ? colors.textDark : colors.textLight,
+                    },
+                }}
             />
-          ),
-          headerRight: () => {
-            return (
-              <TouchableOpacity
-                style={[styles.headerBtnContainer]}
-                onPress={() => router.navigate("/(app)/forms/info-school")}
-                hitSlop={{ top: 25, bottom: 25, left: 25, right: 25 }}
-              >
-                <BookIcon
-                  size={20}
-                  color={themeContext?.isDark ?? false ? "white" : "black"}
-                />
-              </TouchableOpacity>
-            );
-          },
-          headerStyle: {
-            backgroundColor:
-              (themeContext?.isDark
-                ? themeContext?.colors.primarySemiDark
-                : themeContext?.colors.primarySemiLight) ?? "red",
-          },
-          headerTitleStyle: {
-            fontSize: 17,
-            fontFamily: "Raleway-Bold",
-            color: themeContext?.isDark ? colors.textDark : colors.textLight,
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="archive"
-        options={{
-          title: "Archive",
-          headerTitleAlign: "center",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "archive" : "archive-outline"}
-              color={color}
-              focused={focused}
+            <Tabs.Screen
+                name="[calendar]"
+                options={{
+                    title: "Calendrier",
+                    headerTitleAlign: "center",
+                    tabBarIcon: ({color, focused}) => (
+                        <TabBarIcon
+                            name={focused ? "calendar" : "calendar-outline"}
+                            color={color}
+                            focused={focused}
+                        />
+                    ),
+                    headerStyle: {
+                        backgroundColor:
+                            (themeContext?.isDark
+                                ? themeContext?.colors.primarySemiDark
+                                : themeContext?.colors.primarySemiLight) ?? "red",
+                    },
+                    headerTitleStyle: {
+                        fontSize: 17,
+                        fontFamily: "Raleway-Bold",
+                        color: themeContext?.isDark ? colors.textDark : colors.textLight,
+                    },
+                    // Allows to call the route with default parameter 'undefined'
+                    href: {
+                        pathname: "/[calendar]",
+                        params: {
+                            calendar: "undefined",
+                        },
+                    },
+                }}
             />
-          ),
-          headerStyle: {
-            backgroundColor:
-              (themeContext?.isDark
-                ? themeContext?.colors.primarySemiDark
-                : themeContext?.colors.primarySemiLight) ?? "red",
-          },
-          headerTitleStyle: {
-            fontSize: 17,
-            fontFamily: "Raleway-Bold",
-            color: themeContext?.isDark ? colors.textDark : colors.textLight,
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="[calendar]"
-        options={{
-          title: "Calendrier",
-          headerTitleAlign: "center",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "calendar" : "calendar-outline"}
-              color={color}
-              focused={focused}
+            <Tabs.Screen
+                name="account"
+                options={{
+                    title: "Compte",
+                    headerTitleAlign: "center",
+                    tabBarIcon: ({color, focused}) => (
+                        <TabBarIcon name={"body"} color={color} focused={focused}/>
+                    ),
+                    headerRight: () => (
+                        <TouchableOpacity
+                            style={[styles.headerBtnContainer]}
+                            onPress={() => router.navigate("/(app)/(screens)/parameters")}
+                            hitSlop={{top: 25, bottom: 25, left: 25, right: 25}}
+                        >
+                            <BurgerMenuIcon
+                                size={20}
+                                color={themeContext?.isDark ?? false ? "white" : "black"}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerStyle: {
+                        backgroundColor:
+                            (themeContext?.isDark
+                                ? themeContext?.colors.primarySemiDark
+                                : themeContext?.colors.primarySemiLight) ?? "red",
+                    },
+                    headerTitleStyle: {
+                        fontSize: 17,
+                        fontFamily: "Raleway-Bold",
+                        color: themeContext?.isDark ? colors.textDark : colors.textLight,
+                    },
+                }}
             />
-          ),
-          headerStyle: {
-            backgroundColor:
-              (themeContext?.isDark
-                ? themeContext?.colors.primarySemiDark
-                : themeContext?.colors.primarySemiLight) ?? "red",
-          },
-          headerTitleStyle: {
-            fontSize: 17,
-            fontFamily: "Raleway-Bold",
-            color: themeContext?.isDark ? colors.textDark : colors.textLight,
-          },
-          // Allows to call the route with default parameter 'undefined'
-          href: {
-            pathname: "/[calendar]",
-            params: {
-              calendar: "undefined",
-            },
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="account"
-        options={{
-          title: "Compte",
-          headerTitleAlign: "center",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={"body"} color={color} focused={focused} />
-          ),
-          headerRight: () => (
-            <TouchableOpacity
-              style={[styles.headerBtnContainer]}
-              onPress={() => router.navigate("/(app)/(screens)/parameters")}
-              hitSlop={{ top: 25, bottom: 25, left: 25, right: 25 }}
-            >
-              <BurgerMenuIcon
-                size={20}
-                color={themeContext?.isDark ?? false ? "white" : "black"}
-              />
-            </TouchableOpacity>
-          ),
-          headerStyle: {
-            backgroundColor:
-              (themeContext?.isDark
-                ? themeContext?.colors.primarySemiDark
-                : themeContext?.colors.primarySemiLight) ?? "red",
-          },
-          headerTitleStyle: {
-            fontSize: 17,
-            fontFamily: "Raleway-Bold",
-            color: themeContext?.isDark ? colors.textDark : colors.textLight,
-          },
-        }}
-      />
-    </Tabs>
-  );
+        </Tabs>
+    );
 }
--- components/TabBar/TabBar.tsx
+++ components/TabBar/TabBar.tsx
@@ -1,37 +1,38 @@
 import React from "react";
-import { useColorScheme, View } from "react-native";
+import {View} from "react-native";
 import TabBarItem from "@/components/TabBar/TabBarItem";
-import { colors } from "@/config/styles/01-settings/_colors";
+import {BottomTabBarProps} from "@react-navigation/bottom-tabs";
 
 export default function TabBar({
-  state,
-  descriptors,
-  navigation,
-}): React.JSX.Element {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        backgroundColor: "orange", // Enfant orange mais il reste un parent avec background gris
-        borderColor: "pink",
-        borderWidth: 2,
-        zIndex: 5,
-        bottom: 0,
-        height: 100,
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-      }}
-    >
-      {state.routes.map((route, index) => (
-        <TabBarItem
-          key={index + "_" + route.name}
-          index={index}
-          route={route}
-          selectedIndex={state.index}
-          descriptors={descriptors}
-          navigation={navigation}
-        />
-      ))}
-    </View>
-  );
+                                   state,
+                                   descriptors,
+                                   navigation,
+                               }: BottomTabBarProps): React.JSX.Element {
+    return (
+        <View
+            style={{
+                flexDirection: "row",
+                backgroundColor: "orange",
+                borderColor: "pink",
+                borderWidth: 2,
+                zIndex: 5,
+                bottom: 0,
+                height: 100,
+                borderTopLeftRadius: 25,
+                borderTopRightRadius: 25,
+                marginTop: -100 // Margin negative de la même taille que la 'height' pour que la View de l'écran passe derrière la barre de navigation (sorte de absolute sans absolute)
+            }}
+        >
+            {state.routes.map((route, index) => (
+                <TabBarItem
+                    key={index + "_" + route.name}
+                    index={index}
+                    route={route}
+                    selectedIndex={state.index}
+                    descriptors={descriptors}
+                    navigation={navigation}
+                />
+            ))}
+        </View>
+    );
 }
